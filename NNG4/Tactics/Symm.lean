f0957e0215c695a009347b07dcb0afe9523eec63
leningkoei <zhanglening2001@gmail.com>
2025-06-27 14:21:57 +0000

    Bring the true symm tactic!

--- NNG4/Tactics/Symm.lean
+++ NNG4/Tactics/Symm.lean
@@ -1,1 +1,5 @@
 -- Not defined, and don't know how to define.
+
+import Mathlib.Tactic.Tauto
+-- Finally I find `symm` tactic works well after I import this package.
+-- You can find it at `https://github.com/leanprover-community/NNG4/blob/a238f74e546e0a7510f38dc43a202bb165bf9400/Game/Tactic/FromMathlib.lean`.
--- NNG4/Worlds/Implication/OneNeZero.lean
+++ NNG4/Worlds/Implication/OneNeZero.lean
@@ -1,8 +1,11 @@
 import NNG4.Definitions.MyNat
 import NNG4.Tactics.Apply
+import NNG4.Tactics.Symm
 import NNG4.Theorems.ZeroNeOne
 
 theorem one_ne_zero : (1 : ℕ) ≠ 0 := by
-  intro h
-  apply zero_ne_one
-  exact (Eq.symm h)
+  symm
+  exact zero_ne_one
+  -- intro h
+  -- apply zero_ne_one
+  -- exact (Eq.symm h)
